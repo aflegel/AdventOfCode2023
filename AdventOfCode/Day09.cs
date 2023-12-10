01b86d2ea27190537ef5825d29aebfcae3da3ceb
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2023-12-10 21:39:18 +1300

    day9

---
+++ AdventOfCode/Day09.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode;
+
+public class Day09(string input) : IAdventDay
+{
+	private int[][] InputArray { get; } = input.Split("\n").Select(s => s.Split(" ").Select(int.Parse).ToArray()).ToArray();
+
+	public string Part1()
+	{
+		var result = InputArray.Select(s => s[^1] + GetDifference(s)).ToArray();
+		return result.Sum().ToString();
+	}
+
+	private static int GetDifference(int[] series)
+	{
+		var newSeries = series.Skip(1).Select((item, i) => item - series[i]).ToArray();
+
+		return newSeries.All(a => a == 0) ? 0 : newSeries[^1] + GetDifference(newSeries);
+	}
+
+	public string Part2()
+	{
+		var result = InputArray.Select(s => s[0] - GetPreviousDifference(s)).ToArray();
+		return result.Sum().ToString();
+	}
+
+	private static int GetPreviousDifference(int[] series)
+	{
+		var newSeries = series.Skip(1).Select((item, i) => item - series[i]).ToArray();
+		return newSeries.All(a => a == 0) ? 0 : newSeries[0] - GetPreviousDifference(newSeries);
+	}
+}
---
+++ AdventOfCodeTests/Day09Tests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using AdventOfCode;
+using FluentAssertions;
+
+namespace AdventOfCodeTests;
+
+public class Day09Tests
+{
+	private readonly string input = @"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45".ReplaceLineEndings("\n");
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day09(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("114");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day09(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("2");
+	}
+}
