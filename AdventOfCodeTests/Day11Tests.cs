6a02f4cf9ef5aeb35cee3eef5ce868775a7940c6
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2023-12-22 17:58:02 -0600

    day11

---
+++ AdventOfCodeTests/Day11Tests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using AdventOfCode;
+using FluentAssertions;
+
+namespace AdventOfCodeTests;
+
+public class Day11Tests
+{
+	private readonly string input = @"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....".ReplaceLineEndings("\n");
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day11(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("374");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day11(input);
+
+		var answer = day.Part2();
+
+		//factor 2= 374
+		//factor 10 = 1030
+		//factor  100 = 8410
+		answer.Should().Be("8410");
+	}
+}
