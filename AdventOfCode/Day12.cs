62243875ed8646c4d78ae76c69baee00533f424e
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2023-12-24 09:24:36 -0600

    day12-incomplete

---
+++ AdventOfCode/Day12.cs
@@ -0,0 +1,110 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public partial class Day12(string input) : IAdventDay
+{
+	private partial record SpringGroup(string Group, int[] Pattern)
+	{
+		public bool IsValid()
+		{
+			var test = SplitWhenCharactersChange(Group).Where(s => s.Contains('#')).ToArray();
+			if (test.Length != Pattern.Length)
+				return false;
+
+			for (var i = 0; i < test.Length; i++)
+			{
+				if (test[i].Length != Pattern[i])
+					return false;
+			}
+			return true;
+		}
+
+		public bool IsPartiallyValid()
+		{
+			var test = SplitWhenCharactersChange(Group).ToArray();
+			var patternIndex = 0;
+
+			for (var i = 0; i < test.Length; i++)
+			{
+				if (test[i].Contains('?'))
+					return true;
+
+				if (test[i].Contains('#') && patternIndex < Pattern.Length && test[i].Length != Pattern[patternIndex])
+				{
+					//if the next group is unknown it may contain valid input
+					if (i < test.Length - 1 && test[i + 1].Contains('?'))
+						return true;
+					//Console.WriteLine($"{Group} - {string.Join(',', Pattern)}");
+					return false;
+				}
+				else if (test[i].Contains('#'))
+					patternIndex++;
+			}
+			return true;
+		}
+
+		private static string[] SplitWhenCharactersChange(string input)
+			=> CharacterSplit().Matches(input)
+				.Cast<Match>()
+				.Select(m => m.Value).ToArray();
+
+		[GeneratedRegex(@"(.)\1*")]
+		private static partial Regex CharacterSplit();
+
+	}
+	private List<SpringGroup> InputArray { get; } = input.Split("\n").Select(s =>
+	{
+		var split = s.Split(" ");
+		return new SpringGroup(split[0], split[1].Split(",").Select(int.Parse).ToArray());
+	}).ToList();
+
+	public string Part1()
+	{
+		var groups = InputArray.SelectMany(CompleteGroups).ToList();
+		var valid = groups.Where(g => g.IsValid()).ToList();
+		return valid.Count.ToString();
+	}
+
+	private IEnumerable<SpringGroup> CompleteGroups(SpringGroup group)
+	{
+		static string ReplaceFirst(string text, string search, string replace)
+		{
+			var pos = text.IndexOf(search);
+			return pos < 0 ? text : text[..pos] + replace + text[(pos + search.Length)..];
+		}
+
+		if (!group.Group.Contains('?', StringComparison.CurrentCulture))
+			yield return group;
+		else
+		{
+			var a = new SpringGroup(ReplaceFirst(group.Group, "?", "."), group.Pattern);
+			var b = new SpringGroup(ReplaceFirst(group.Group, "?", "#"), group.Pattern);
+
+			if (a.IsPartiallyValid())
+				foreach (var g in CompleteGroups(a))
+					yield return g;
+
+			if (b.IsPartiallyValid())
+				foreach (var g in CompleteGroups(b))
+					yield return g;
+		}
+	}
+
+	public string Part2()
+	{
+		var data = GetUnfoldedResults();
+
+		var groups = data.SelectMany(CompleteGroups).ToList();
+		var valid = groups.Where(g => g.IsValid()).ToList();
+		return valid.Count.ToString();
+	}
+
+	private List<SpringGroup> GetUnfoldedResults()
+	{
+		var groups = InputArray.Select(s => new SpringGroup(string.Join("?", Enumerable.Repeat(s.Group, 5)), Enumerable.Repeat(s.Pattern, 5).SelectMany(s => s).ToArray())).ToList();
+
+		return groups;
+	}
+
+}
---
+++ AdventOfCodeTests/Day12Tests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using AdventOfCode;
+using FluentAssertions;
+
+namespace AdventOfCodeTests;
+
+public class Day12Tests
+{
+	private readonly string input = @"???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1".ReplaceLineEndings("\n");
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day12(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("21");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day12(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("525152");
+	}
+}
