a308299bce89861a0cc31df6259c685cef70a26f
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2023-12-25 15:45:02 -0600

    day13

---
+++ AdventOfCodeTests/Day13Tests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using AdventOfCode;
+using FluentAssertions;
+
+namespace AdventOfCodeTests;
+
+public class Day13Tests
+{
+	private readonly string input = @"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#".ReplaceLineEndings("\n");
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day13(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("405");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day13(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("400");
+	}
+}
